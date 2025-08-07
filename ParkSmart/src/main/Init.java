4ab1635ee50e5a2113e539600ef26a998de31cbe
khuya <khuya@10.0.0.228>
2025-08-06 19:32:19 -0500

    added ParkSmart logo to welcome screen

--- ParkSmart/src/main/Init.java
+++ ParkSmart/src/main/Init.java
@@ -8,16 +8,26 @@ public class Init {
 
     public static void main(String[] args) {
     	ParkingManager.init();
+    	
         JFrame frame = new JFrame("ParkSmart - Home");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        frame.setSize(400, 500);
         frame.setLayout(new BorderLayout());
 
+        // Welcome Page
         JLabel titleLabel = new JLabel("Welcome to ParkSmart", SwingConstants.CENTER);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
         titleLabel.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
         frame.add(titleLabel, BorderLayout.NORTH);
+        
+        //Load Logo from resources
+        ImageIcon logoIcon = new ImageIcon(Init.class.getResource("/resources/images/project_logo.png"));
+        Image scaledLogo = logoIcon.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        JLabel logoLabel = new JLabel(new ImageIcon(scaledLogo));
+        logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        logoLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
+        //Buttons below Logo
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
@@ -45,7 +55,9 @@ public class Init {
         buttonPanel.add(Box.createVerticalStrut(20));
         buttonPanel.add(managerButton);
 
-        frame.add(buttonPanel, BorderLayout.CENTER);
+        frame.add(logoLabel, BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+        
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }   
