efd0dddad2a9489b7da7fb8f7c69481910957115
khuya <khuya@10.0.0.228>
2025-08-07 09:26:06 -0500

    Removed login/manager buttons and added clickable project logo to redirect user to login page

--- ParkSmart/src/main/Init.java
+++ ParkSmart/src/main/Init.java
@@ -3,6 +3,8 @@ package main;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Init {
 
@@ -20,43 +22,71 @@ public class Init {
         titleLabel.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
         frame.add(titleLabel, BorderLayout.NORTH);
         
-        //Load Logo from resources
+        //Load Logo
         ImageIcon logoIcon = new ImageIcon(Init.class.getResource("/resources/images/project_logo.png"));
         Image scaledLogo = logoIcon.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
         JLabel logoLabel = new JLabel(new ImageIcon(scaledLogo));
         logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
         logoLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        
+        
+        
+        //Logo is clickable --> redirects user to login screen
+        logoLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                frame.dispose();          // Close welcome window
+                UserLogin.launch();       // Open login screen
+            }
 
-        //Buttons below Logo
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                logoLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // Show hand icon
+            }
+        });
+
+        frame.add(logoLabel, BorderLayout.CENTER);
+        
+        
+
+        /*
+        //User Login below Logo
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 50, 20, 50));
 
         JButton userButton = new JButton("Login as User");
-        JButton managerButton = new JButton("Login as Manager");
+        //JButton managerButton = new JButton("Login as Manager");
 
         userButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        managerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //managerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         userButton.setMaximumSize(new Dimension(200, 40));
-        managerButton.setMaximumSize(new Dimension(200, 40));
+        //managerButton.setMaximumSize(new Dimension(200, 40));
 
         userButton.addActionListener((ActionEvent e) -> {
             frame.dispose();
             UserLogin.launch(); // Launches user login screen
         });
+        */
 
+        /*
         managerButton.addActionListener((ActionEvent e) -> {
             frame.dispose();
             Manager.launch();
         });
-
+        */
+        
+        
+        /*
         buttonPanel.add(userButton);
-        buttonPanel.add(Box.createVerticalStrut(20));
-        buttonPanel.add(managerButton);
+        //buttonPanel.add(Box.createVerticalStrut(20));
+        //buttonPanel.add(managerButton);
 
-        frame.add(logoLabel, BorderLayout.CENTER);
+        //frame.add(logoLabel, BorderLayout.CENTER);
         frame.add(buttonPanel, BorderLayout.SOUTH);
+        */
+        
         
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
